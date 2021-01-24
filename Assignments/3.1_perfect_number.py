dfedc75197e378c57fc6c252a02226222d362775
Kirshoff <fatihfidan52@gmail.com>
2021-01-24 20:47:14 +0100

    up

--- Assignments/3.1_perfect_number.py
+++ Assignments/3.1_perfect_number.py
@@ -9,6 +9,8 @@ Some other perfect numbers are `28(1+2+4+7+14=28)`, `496` and `8128`.
 Write a function that finds perfect numbers between `1` and `1000`.
 Check perfect numbers between `1` and `1000` and find the sum of the perfect numbers using reduce and filter functions. <br />
 """
+from functools import reduce
+
 def isperfect(num):
     multipliers = [i+1 for i in range(num//2) if num%(i+1)==0]
     return sum(multipliers)==num
